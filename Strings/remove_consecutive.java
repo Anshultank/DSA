f161aee2699a8b91d1d971f0810f06c756feb324
Anshul Tank <92208668+Anshultank@users.noreply.github.com>
2022-06-21 23:41:16 +0530

    Add files via upload
    strings started

---
+++ Strings/remove_consecutive.java
@@ -0,0 +1,11 @@
+package Strings;
+import java.util.Scanner;
+public class remove_consecutive{
+    public String remove_consecutive(String S){
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
+
+
+
